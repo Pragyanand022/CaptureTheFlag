5b25eda491838944fd26e0a16e6597b5931057b8
Pragyanand022 <99154355+Pragyanand022@users.noreply.github.com>
2022-12-23 22:21:56 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+const mobile_nav = document.getElementById("mobnavid");
+const nav_header = document.getElementById("smallNav");
+// console.log(mobile_nav);
+// console.log(nav_header);
+function toggleNavbar(){
+    console.log(23)
+    console.log("hello world");
+    // alert("plz here");
+    nav_header.classList.add("active");
+    console.log(24);
+    
+}
+
+function toggleNavbar2(){
+    console.log(23)
+    console.log("hello world");
+    // alert("plz here");
+    nav_header.classList.remove("active");
+    console.log(24);
+    
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+var len = acc.length;
+for (i = 0; i < len; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");    
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
